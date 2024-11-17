6d52dce0e6d4e610ad797c68c5d65c02e65f9bd1
Daniel Isaksson <Daniel.Isaksson@yh.Nackademin.se>
2024-11-17 15:03:11 +0100

    Added and moved adding dates to its own controller

---
+++ src/main/java/com/example/demo/controller/admin/AddDatesController.java
@@ -0,0 +1,98 @@
+package com.example.demo.controller.admin;
+
+import com.example.demo.model.BookableDate;
+import com.example.demo.model.BookableHour;
+import com.example.demo.services.BookableDateService;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Controller
+@RequestMapping("/add-dates")
+@PreAuthorize("hasAuthority('Admin')")
+public class AddDatesController {
+
+    private final BookableDateService bookableDateService;
+    public AddDatesController(BookableDateService bookableDateService){
+        this.bookableDateService = bookableDateService;
+    }
+    @RequestMapping("/")
+    public String addDates(){
+        return "add-available-dates";
+    }
+
+    @RequestMapping("/confirm-dates")
+    public String confirmDatesToAdd(@RequestParam List<String> time, @RequestParam String fromDate,
+                                    @RequestParam String toDate, Model model){
+        List<LocalDate> availableDates = new ArrayList<>();
+
+        for (LocalDate date = LocalDate.parse(fromDate); !date.isAfter(LocalDate.parse(toDate));
+             date = date.plusDays(1)) {
+            if (date.getDayOfWeek() != DayOfWeek.SUNDAY) {
+                availableDates.add(date);
+            }
+        }
+        model.addAttribute("dates", availableDates);
+        model.addAttribute("times", time);
+        return "confirm-dates";
+    }
+
+    @PostMapping("/save-dates")
+    public String saveDates(@RequestParam List<String> dates, Model model) {
+        Map<String, List<String>> datesWithHours = createDatesWithHoursMap(dates);
+
+        List<BookableDate> datesToSave = new ArrayList<>();
+        for (Map.Entry<String, List<String>> entry : datesWithHours.entrySet()) {
+            List<BookableHour> hoursToSave = new ArrayList<>();
+            for(String s: entry.getValue()){
+                hoursToSave.add(BookableHour.builder()
+                        .hour(LocalTime.parse(s))
+                        .booked(false)
+                        .build());
+            }
+            BookableDate dateToSave = BookableDate.builder()
+                    .date(LocalDate.parse(entry.getKey()))
+                    .bookableHours(hoursToSave)
+                    .fullyBooked(false)
+                    .build();
+            datesToSave.add(dateToSave);
+        }
+
+        bookableDateService.saveAllBookableDatesAndAssociatedHours(datesToSave);
+
+        model.addAttribute("datesAdded", datesToSave.size() + " dates added");
+        return "admin-landing-page";
+    }
+
+    private static Map<String, List<String>> createDatesWithHoursMap(List<String> dates) {
+        String oldValue = "nothing";
+        Map<String, List<String>> datesWithHours = new HashMap<>();
+
+        for(String s: dates){
+            String date = s.split("=")[0];
+            String time = s.split("=")[1];
+            if(date.equals(oldValue)){
+                List<String> times = datesWithHours.get(date);
+                times.add(time);
+            }else{
+                List<String> newTimesList = new ArrayList<>();
+                newTimesList.add(time);
+                datesWithHours.put(date, newTimesList);
+            }
+            oldValue = date;
+        }
+        return datesWithHours;
+    }
+
+}
--- src/main/java/com/example/demo/controller/admin/BookingController.java
+++ src/main/java/com/example/demo/controller/admin/BookingController.java
@@ -1,11 +1,8 @@
 package com.example.demo.controller.admin;
 
-import com.example.demo.model.BookableDate;
-import com.example.demo.model.BookableHour;
 import com.example.demo.model.Booking;
 import com.example.demo.model.Customer;
 import com.example.demo.model.dtos.bookingdtos.BookingCustomerDepositTimeDto;
-import com.example.demo.services.BookableDateService;
 import com.example.demo.services.BookingService;
 import com.example.demo.services.CustomerService;
 import com.example.demo.util.calendar.CalendarService;
@@ -17,13 +14,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.time.LocalTime;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 @Controller
 @RequestMapping("/booking")
@@ -32,13 +24,12 @@ public class BookingController {
     private final CalendarService calendarService;
     private final BookingService bookingService;
     private final CustomerService customerService;
-    private final BookableDateService bookableDateService;
+
     public BookingController(CalendarService calendarService, BookingService bookingService,
-                             CustomerService customerService, BookableDateService bookableDateService){
+                             CustomerService customerService){
         this.calendarService = calendarService;
         this.bookingService = bookingService;
         this.customerService = customerService;
-        this.bookableDateService = bookableDateService;
     }
 
 
@@ -118,74 +109,6 @@ public class BookingController {
         model.addAttribute("successDate", date);
         model.addAttribute("successTime", time);
         return "book-tattoo-with-date";
-    }
-
-    @RequestMapping("/add-dates")
-    public String addDates(){
-        return "add-available-dates";
-    }
-
-    @RequestMapping("/confirm-dates")
-    public String confirmDatesToAdd(@RequestParam List<String> time, @RequestParam String fromDate,
-                                    @RequestParam String toDate, Model model){
-        List<LocalDate> availableDates = new ArrayList<>();
-
-        for (LocalDate date = LocalDate.parse(fromDate); !date.isAfter(LocalDate.parse(toDate));
-             date = date.plusDays(1)) {
-            if (date.getDayOfWeek() != DayOfWeek.SUNDAY) {
-                availableDates.add(date);
-            }
-        }
-        model.addAttribute("dates", availableDates);
-        model.addAttribute("times", time);
-        return "confirm-dates";
-    }
-
-    @PostMapping("/save-dates")
-    public String saveDates(@RequestParam List<String> dates, Model model) {
-        Map<String, List<String>> datesWithHours = createDatesWithHoursMap(dates);
-
-        List<BookableDate> datesToSave = new ArrayList<>();
-        for (Map.Entry<String, List<String>> entry : datesWithHours.entrySet()) {
-            List<BookableHour> hoursToSave = new ArrayList<>();
-            for(String s: entry.getValue()){
-                hoursToSave.add(BookableHour.builder()
-                        .hour(LocalTime.parse(s))
-                        .booked(false)
-                        .build());
-            }
-            BookableDate dateToSave = BookableDate.builder()
-                    .date(LocalDate.parse(entry.getKey()))
-                    .bookableHours(hoursToSave)
-                    .fullyBooked(false)
-                    .build();
-            datesToSave.add(dateToSave);
-        }
-
-        bookableDateService.saveAllBookableDatesAndAssociatedHours(datesToSave);
-
-        model.addAttribute("datesAdded", datesToSave.size() + " dates added");
-        return "admin-landing-page";
-    }
-
-    private static Map<String, List<String>> createDatesWithHoursMap(List<String> dates) {
-        String oldValue = "nothing";
-        Map<String, List<String>> datesWithHours = new HashMap<>();
-
-        for(String s: dates){
-            String date = s.split("=")[0];
-            String time = s.split("=")[1];
-            if(date.equals(oldValue)){
-                List<String> times = datesWithHours.get(date);
-                times.add(time);
-            }else{
-                List<String> newTimesList = new ArrayList<>();
-                newTimesList.add(time);
-                datesWithHours.put(date, newTimesList);
-            }
-            oldValue = date;
-        }
-        return datesWithHours;
     }
 
 }

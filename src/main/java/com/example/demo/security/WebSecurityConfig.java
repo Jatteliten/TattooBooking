b9627906fba8b6c9944284cd18282f2ff1b76949
Daniel Isaksson <Daniel.Isaksson@yh.Nackademin.se>
2025-02-07 13:50:59 +0100

    Added layouts-folder for html templates

--- src/main/java/com/example/demo/security/WebSecurityConfig.java
+++ src/main/java/com/example/demo/security/WebSecurityConfig.java
@@ -39,7 +39,7 @@ public class WebSecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests((requests) -> requests
-                        .requestMatchers("/", "/about-me", "/booking-calendar", "/js/**", "/css/**", "/images/**", "/login/**", "/logout")
+                        .requestMatchers("/", "/layouts", "/about-me", "/booking-calendar", "/js/**", "/css/**", "/images/**", "/login/**", "/logout")
                         .permitAll()
                         .anyRequest().authenticated()
                 )

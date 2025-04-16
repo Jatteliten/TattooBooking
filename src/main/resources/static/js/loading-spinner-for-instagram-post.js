b2e9e927b56c49c48119ffb2d81975d429496d35
Daniel Isaksson <Daniel.Isaksson@yh.Nackademin.se>
2025-04-16 12:07:09 +0200

    Spinner set to shorter "loading"

--- src/main/resources/static/js/loading-spinner-for-instagram-post.js
+++ src/main/resources/static/js/loading-spinner-for-instagram-post.js
@@ -2,5 +2,5 @@ window.addEventListener('load', function () {
     setTimeout(function () {
         document.getElementById('loading').style.display = 'none';
         document.getElementById('instagram-content').classList.add('visible');
-    }, 200);
+    }, 100);
 });

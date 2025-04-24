68fe7a6bdcdff77811757f3c25cd6fec5b79446e
Daniel Isaksson <Daniel.Isaksson@yh.Nackademin.se>
2025-04-24 13:12:29 +0200

    Fixed url issues when trying to delete image from s3 bucket.

--- src/main/java/com/example/demo/services/S3ImageService.java
+++ src/main/java/com/example/demo/services/S3ImageService.java
@@ -6,11 +6,12 @@ import org.springframework.web.multipart.MultipartFile;
 import software.amazon.awssdk.core.sync.RequestBody;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.model.DeleteObjectRequest;
-import software.amazon.awssdk.services.s3.model.GetUrlRequest;
 import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardCopyOption;
@@ -46,19 +47,17 @@ public class S3ImageService {
                     RequestBody.fromInputStream(inputStream, Files.size(tempFile)));
         }
 
-        GetUrlRequest getUrlRequest = GetUrlRequest.builder()
-                .bucket(bucketName)
-                .key(s3Key)
-                .build();
-
-        return s3Client.utilities().getUrl(getUrlRequest).toString();
+        return "https://" + bucketName + ".s3.eu-north-1.amazonaws.com/" + s3Key;
     }
+
 
     public void deleteImage(String imageUrl) {
         String key = imageUrl.substring(imageUrl.indexOf(".com/") + 5);
+
+        String decodedKey = URLDecoder.decode(key, StandardCharsets.UTF_8);
         DeleteObjectRequest deleteRequest = DeleteObjectRequest.builder()
                 .bucket(bucketName)
-                .key(key)
+                .key(decodedKey)
                 .build();
 
         s3Client.deleteObject(deleteRequest);

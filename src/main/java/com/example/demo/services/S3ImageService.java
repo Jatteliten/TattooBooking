4c64070659fa057d696ad345d8185e70ebaf8332
Daniel Isaksson <Daniel.Isaksson@yh.Nackademin.se>
2025-04-25 10:15:26 +0200

    Added deleteImages, to be able to delete a list of images.

--- src/main/java/com/example/demo/services/S3ImageService.java
+++ src/main/java/com/example/demo/services/S3ImageService.java
@@ -5,7 +5,10 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 import software.amazon.awssdk.core.sync.RequestBody;
 import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.Delete;
 import software.amazon.awssdk.services.s3.model.DeleteObjectRequest;
+import software.amazon.awssdk.services.s3.model.DeleteObjectsRequest;
+import software.amazon.awssdk.services.s3.model.ObjectIdentifier;
 import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 
 import java.io.IOException;
@@ -15,7 +18,9 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardCopyOption;
+import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Service
 public class S3ImageService {
@@ -61,5 +66,22 @@ public class S3ImageService {
                 .build();
 
         s3Client.deleteObject(deleteRequest);
+    }
+
+    public void deleteImages(List<String> imageUrls) {
+        List<ObjectIdentifier> objectsToDelete = imageUrls.stream()
+                .map(url -> {
+                    String key = url.substring(url.indexOf(".com/") + 5);
+                    String decodedKey = URLDecoder.decode(key, StandardCharsets.UTF_8);
+                    return ObjectIdentifier.builder().key(decodedKey).build();
+                })
+                .collect(Collectors.toList());
+
+        DeleteObjectsRequest deleteRequest = DeleteObjectsRequest.builder()
+                .bucket(bucketName)
+                .delete(Delete.builder().objects(objectsToDelete).build())
+                .build();
+
+        s3Client.deleteObjects(deleteRequest);
     }
 }

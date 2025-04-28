48bc5fe1a3f4b11b7552e682bb66447d8413a8d0
Daniel Isaksson <Daniel.Isaksson@yh.Nackademin.se>
2025-04-28 14:14:40 +0200

    Removed unused modal js.
    Removed unused properties in test.properties.

--- src/main/resources/static/js/dynamically-add-img-src-to-modal-in-booking-view.js
+++
@@ -1,5 +0,0 @@
-function showImageInModal(element) {
-    const imageUrl = element.getAttribute("data-image-src");
-    const modalImage = document.getElementById("modalImage");
-    modalImage.src = imageUrl;
-}

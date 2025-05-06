53074dd37115ce9338cc6b149453e868855b6b05
Daniel Isaksson <Daniel.Isaksson@yh.Nackademin.se>
2025-05-06 20:29:12 +0200

    Added arrows to image modal that lets you change image in list by a button press.

--- src/main/resources/static/js/display-image-modal.js
+++ src/main/resources/static/js/display-image-modal.js
@@ -1,12 +1,51 @@
 let imageModal = document.getElementById('imageModal');
+let modalImage = imageModal.querySelector('#modalImage');
+let modalCaption = imageModal.querySelector('#modalCaption');
+let nextBtn = document.getElementById('nextImage');
+let prevBtn = document.getElementById('prevImage');
+
+let images = [];
+let captions = [];
+let currentIndex = 0;
+
 imageModal.addEventListener('show.bs.modal', function (event) {
     let button = event.relatedTarget;
+
+    images = Array.from(document.querySelectorAll('[data-bs-image]')).map(btn => btn.getAttribute('data-bs-image'));
+    captions = Array.from(document.querySelectorAll('[data-bs-image]')).map(btn => btn.getAttribute('data-bs-caption'));
+
     let imageUrl = button.getAttribute('data-bs-image');
-    let captionText = button.getAttribute('data-bs-caption');
 
-    let modalImage = imageModal.querySelector('#modalImage');
-    let modalCaption = imageModal.querySelector('#modalCaption');
+    currentIndex = images.indexOf(imageUrl);
 
-    modalImage.src = imageUrl;
-    modalCaption.textContent = captionText;
+    showImage();
+});
+
+function showImage() {
+    if (images.length <= 1) {
+        nextBtn.style.display = 'none';
+        prevBtn.style.display = 'none';
+    } else {
+        nextBtn.style.display = 'inline-block';
+        prevBtn.style.display = 'inline-block';
+    }
+
+    if (images.length > 0) {
+        modalImage.src = images[currentIndex];
+        modalCaption.textContent = captions[currentIndex];
+    }
+}
+
+nextBtn.addEventListener('click', function () {
+    if (images.length > 0) {
+        currentIndex = (currentIndex + 1) % images.length;
+        showImage();
+    }
+});
+
+prevBtn.addEventListener('click', function () {
+    if (images.length > 0) {
+        currentIndex = (currentIndex - 1 + images.length) % images.length;
+        showImage();
+    }
 });
